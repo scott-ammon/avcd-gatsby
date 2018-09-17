a5e749f1b468f1617e95e6d0a434fc5469cb931f
Scott Ammon <scott.o.ammon@gmail.com>
2018-09-17 13:51:02 -0700

    added testimonial content from site

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,10 +3,28 @@ import Link from 'gatsby-link'
 
 const ProjectsPage = () => (
   <div>
-    <h1>Projects</h1>
-    <p>Project 1</p>
-    <p>Project 2</p>
-    <p>Project 3</p>
+    <h1>Select Projects</h1>
+    <p>
+      Many of our projects cannot be shown, out of respect for our clients’ privacy. We welcome 
+      you to contact us with specific inquiries about the wide range of yachts, private residences, 
+      and other locations in which we have worked for over 20 years.
+    </p>
+    <p>M/Y Adler</p>
+    <p>M/Y Big Fish</p>
+    <p>M/Y Cakewalk 62m</p>
+    <p>M/Y Cakewalk 85m</p>
+    <p>Glendinning</p>
+    <p>M/Y Happy Days</p>
+    <p>M/Y Illusion</p>
+    <p>M/Y Kisses</p>
+    <p>LWR Testimony</p>
+    <p>M/Y Miss Lisa</p>
+    <p>M/Y Marama</p>
+    <p>M/Y Meduse</p>
+    <p>M/Y Mr. Terrible</p>
+    <p>M/Y Phoenix</p>
+    <p>M/Y Polar Bear</p>
+    <p>M/Y Slojo</p>
   </div>
 )
 
