6adf19e82462eeb123f4a1becfced333b7ba83f7
Scott Ammon <scott.o.ammon@gmail.com>
2018-09-24 17:50:57 -0700

    site wide styles for navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,63 @@
 import React, { Component } from 'react'
 import Link from 'gatsby-link'
 import styles from '../styles/navbar.module.css'
-import logo from '../img/avcd-logo2.png'
+import logoWhite from '../img/avcd-logo1.png'
+import logoBlack from '../img/avcd-logo2.png'
 import classNames from 'classnames'
 
 class Navbar extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      nav: styles.headerOne
+    let pageName = window.location.href.split('/');
+    let linkStyle;
+    let iconStyle;
+    let navStyle;
+
+    if(pageName[pageName.length - 1] === '') {
+      navStyle = styles.headerOne;
+      iconStyle = logoWhite;
+      linkStyle = styles.navWhite;
+    } else {
+      navStyle = styles.headerTwo;
+      iconStyle = logoBlack;
+      linkStyle = styles.navBlack; 
     }
+    this.state = {
+      nav: navStyle,
+      icon: iconStyle,
+      link: linkStyle
+    }
+    this.handleClick = this.handleClick.bind(this)
     this.handleScroll = this.handleScroll.bind(this)
   }
 
   handleScroll = (e) => {
-    if(window.pageYOffset > 0) {
-      this.setState({
-        nav: styles.headerTwo
-      }) 
-    } else if (window.pageYOffset === 0) {
-      this.setState({
-        nav: styles.headerOne
-      })
+
+    let pageName = window.location.href.split('/');
+
+    if(pageName[pageName.length - 1] === '') {
+      if(window.pageYOffset > 0) {
+        this.setState({
+          nav: styles.headerTwo,
+          icon: logoBlack,
+          link: styles.navBlack
+        }) 
+      } else if (window.pageYOffset === 0) {
+        this.setState({
+          nav: styles.headerOne,
+          icon: logoWhite,
+          link: styles.navWhite
+        })
+      }
     }
+  }
+
+  handleClick = (e) => {
+    this.setState({
+      nav: styles.headerTwo,
+      icon: logoBlack,
+      link: styles.navBlack
+    }) 
   }
 
   componentDidMount() {
@@ -30,14 +65,16 @@ class Navbar extends Component {
   }
 
   render() {
+    console.log(window.location.href)
+
     return (
       <div className={classNames(this.state.nav, styles.nav)}>
-        <Link to="/"><img className={styles.logo} src={logo}></img></Link> 
+        <Link to="/"><img className={styles.logo} src={this.state.icon}></img></Link> 
         <div className={styles.headerRight}>
-          <Link to="/about" className={styles.navLink} activeClassName={styles.active}>ABOUT</Link>
-          <Link to="/projects" className={styles.navLink} activeClassName={styles.active}>PROJECTS</Link>
-          <Link to="/testimonials" className={styles.navLink} activeClassName={styles.active}>TESTIMONIALS</Link>
-          <Link to="/contact" className={styles.navLink} activeClassName={styles.active}>CONTACT</Link>
+          <Link to="/about" onClick={this.handleClick} className={classNames(this.state.link, styles.navLink)} activeClassName={styles.active}>About</Link>
+          <Link to="/projects" onClick={this.handleClick} className={classNames(this.state.link, styles.navLink)} activeClassName={styles.active}>Projects</Link>
+          <Link to="/testimonials" onClick={this.handleClick} className={classNames(this.state.link, styles.navLink)} activeClassName={styles.active}>Testimonials</Link>
+          <Link to="/contact" onClick={this.handleClick} className={classNames(this.state.link, styles.navLink)} activeClassName={styles.active}>Contact</Link>
         </div>
       </div>
     )
