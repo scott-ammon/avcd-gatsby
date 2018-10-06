17f4f303ac048c35a764212b8d6ec33dc069a4cc
Scott Ammon <scott.o.ammon@gmail.com>
2018-10-06 13:42:03 -0700

    fixed contact logo

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -21,55 +21,57 @@ class ContactPage extends Component {
       <div className={styles.container}>
         <h1 className={styles.title}>Contact Us</h1>
         <p className={styles.tagline}>Don't hesitate to reach out to us. We'll get back to you shortly!</p>
-        <form className={styles.contactFlex} 
-              method="POST"
-              action="https://formspree.io/scott.o.ammon@gmail.com"
-              name="info-form"
-        >
-          <label>*Full Name</label>
-          <input
-            name="name"
-            type="text"
-            className={styles.contactInput}
-            required
-          />
-          <label>*Email</label>
-          <input
-            name="email"
-            type="text"
-            className={styles.contactInput}
-            required
-          />
-          <label>Phone</label>
-          <input
-            name="phone"
-            type="text"
-            className={styles.contactInput}
-          />
-          <label>Select Project Type:</label>
-          <select>
-            <option value="marine-newbuild">Marine New Build</option>
-            <option value="marine-refit">Marine Refit</option>
-            <option value="residential-new">Residential New</option>
-            <option value="residential-remodel">Residential Remodel</option>
-          </select>
-          <label>Desired Project Timing</label>
-          <input
-            name="timing"
-            type="text"
-            className={styles.contactInput}
-          />
-          <label>Comments</label>
-          <textarea
-            name="comments"
-            type="text"
-            rows="5"
-            className={styles.contactInput}
-          />
-          <p>*Indicates required field</p>
-          <button>Submit</button>
-        </form>
-        {/* <img src={logo} className={styles.logo}></img> */}
+        <div className={styles.logoWrapper}>
+          <form className={styles.contactFlex} 
+                method="POST"
+                action="https://formspree.io/scott.o.ammon@gmail.com"
+                name="info-form"
+          >
+            <label>*Full Name</label>
+            <input
+              name="name"
+              type="text"
+              className={styles.contactInput}
+              required
+            />
+            <label>*Email</label>
+            <input
+              name="email"
+              type="text"
+              className={styles.contactInput}
+              required
+            />
+            <label>Phone</label>
+            <input
+              name="phone"
+              type="text"
+              className={styles.contactInput}
+            />
+            <label>Select Project Type:</label>
+            <select>
+              <option value="marine-newbuild">Marine New Build</option>
+              <option value="marine-refit">Marine Refit</option>
+              <option value="residential-new">Residential New</option>
+              <option value="residential-remodel">Residential Remodel</option>
+            </select>
+            <label>Desired Project Timing</label>
+            <input
+              name="timing"
+              type="text"
+              className={styles.contactInput}
+            />
+            <label>Comments</label>
+            <textarea
+              name="comments"
+              type="text"
+              rows="5"
+              className={styles.contactInput}
+            />
+            <p>*Indicates required field</p>
+            <button>Submit</button>
+          </form>
+          <img src={logo} className={styles.logo}></img>
+        </div>
       </div>
     )
   }
