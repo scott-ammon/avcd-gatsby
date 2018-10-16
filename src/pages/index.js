d68c47367f9f20fdd0894547642e39c1002f1fa0
Scott Ammon <scott.o.ammon@gmail.com>
2018-10-16 11:35:57 -0700

    fixed homepage styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styles from '../styles/index.module.css'
+import classNames from 'classnames'
 import logo from '../img/avcdWhiteSmall.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLock } from '@fortawesome/free-solid-svg-icons'
@@ -12,6 +13,9 @@ import meaculpa from '../img/meaculpa.jpg'
 import savannah from '../img/savannah.jpg'
 import tv from '../img/tv.jpg'
 import yacht2 from '../img/yacht2.jpeg'
+import muchoMasSalon from '../img/mucho-mas-salon.jpg'
+import cabin from '../img/cabin.jpg'
+import maramarack from '../img/maramarack2.jpg'
 
 import * as Scroll from 'react-scroll'
 const Element = Scroll.Element;
@@ -62,17 +66,16 @@ class IndexPage extends Component {
               </div>
               <div className={styles.testimonial}>
                 <p>
-                  "Audio Visual Concepts & Design came onboard to improve the archaic control system of CV-9’s 
-                  entertainment system that included Kaleidescape Movie & Music server, TVRO antenna & service, 
-                  local disc players, and a large array amplifiers and speakers throughout the yacht. That was 
-                  the first time for me where a AV technician could actually describe to me how the system was 
-                  going to operate. In short order we had iPad controls, improved speaker systems and also detailed 
-                  drawings of the entire system where the crew could easily be supported at a later time. For the next 
-                  five years of heavy use we rarely had any issues to utilize that aspect." <br></br>– Captain Ethan Lee, M/Y CV-9 (2008-2016)  
+                “AVCD designed, installed and have been responsible for the maintenance of the AV and IT 
+                infrastructure on board M/Y MUCHOS MAS. There work has always been of the highest caliber 
+                and continue to support the vessel at all times to troubleshoot any issues that arise on 
+                board and have been a pleasure to work with both during the yacht build, and supporting the 
+                yacht’s day-to-day operations. I would recommend AVCD to any yacht Captain or management 
+                company looking to update, refit, or new build design and install. <br></br>- Captain Troy Dougherty, M/Y Muchos Mas.
                 </p>
               </div>
               <div className={styles.flexCapabilityTwo}>
-                <img src={tv} className={styles.imageTwo}></img>
+                <img src={muchoMasSalon} className={styles.imageTwo}></img>
                 <div className={styles.boxSupport}>
                   <h2>Who We Support</h2>
                   <h4>Marine</h4>
@@ -83,7 +86,7 @@ class IndexPage extends Component {
                   </ul>
                   <h4>Residential</h4>
                   <ul>
-                    <li>Single Dwelling</li>
+                    <li>Single Dwellings</li>
                     <li>Estates</li>
                     <li>Vacation Homes</li>
                     <li>Lodges</li>
@@ -92,10 +95,13 @@ class IndexPage extends Component {
               </div>
               <div className={styles.testimonial}>
                 <p>
-                  "Entertainment systems are so advanced you absolutely need the most knowledgeable system designer and support available.  
-                  AV Concepts has proven time and time again the ability to bring together a world of entertainment that caters to the customer’s needs.  
-                  Knowledge and support have been key factors to keeping things up to date and performing with outstanding results. After working 
-                  with Steve & Cindy on both new & refit projects for many years it has been a pleasure to have their support.” <br></br>- Captain John Doty, M/Y Onika
+                  "Audio Visual Concepts & Design came onboard to improve the archaic control system of CV-9’s 
+                  entertainment system that included Kaleidescape Movie & Music server, TVRO antenna & service, 
+                  local disc players, and a large array amplifiers and speakers throughout the yacht. That was 
+                  the first time for me where a AV technician could actually describe to me how the system was 
+                  going to operate. In short order we had iPad controls, improved speaker systems and also detailed 
+                  drawings of the entire system where the crew could easily be supported at a later time. For the next 
+                  five years of heavy use we rarely had any issues to utilize that aspect." <br></br>– Captain Ethan Lee, M/Y CV-9  
                 </p>
               </div>
               <div className={styles.flexCapability}>
@@ -112,7 +118,17 @@ class IndexPage extends Component {
                     <li>Ongoing Support 24/7</li>
                   </ul>
                 </div>
-                <img src={yacht2} className={styles.imageThree}></img>
+                <img src={cabin} className={styles.imageThree}></img>
+              </div>
+              <div className={classNames(styles.testimonial, styles.finalTestimonial)}>
+                <p>
+                  “I am the Captain of a 138ft M/Y that had the pleasure of working with AVDC during a complete refit of 
+                  our yacht.  Throughout the process ACDV remained easily contactable, informative and flexible due to constant 
+                  changes being made to the project and the scale of work dramatically increasing.  The professionalism did not 
+                  end once installation had taken place and they have been onboard for any warranty issues as well as changes 
+                  at the owners request.  I would highly recommend them for any up and coming projects that need to be carried 
+                  out to a ‘super yacht’ standard and with professionalism that surpasses all other companies in the yacht business.” <br></br>- Captain Christopher Grant, M/Y Mea Culpa
+                </p>
               </div>
             </div>
           <div className={styles.container}>
@@ -157,23 +173,24 @@ class IndexPage extends Component {
               </div>
             </div>
             <hr></hr>
-            <div className={styles.support}>
-              <h2>Trusted Support</h2>
-              <p>Customer Service is what makes A/V Concepts and Design stand out from our competitors.</p>
-              <p> 
-                Neither the size nor the scope of a project is our concern, rather it is our professional manner, 
-                and our proven ability to work with the project team to get the job done on time, and as expected. 
-                We offer 24 hour, year-round service and support.
-              </p>
-              <p>Worldwide Support. Representation in Washington & Florida.</p>
-              <p className={styles.quote}>
-              “I am the Captain of a 138ft M/Y that had the pleasure of working with AVDC during a complete refit of 
-              our yacht.  Throughout the process ACDV remained easily contactable, informative and flexible due to constant 
-              changes being made to the project and the scale of work dramatically increasing.  The professionalism did not 
-              end once installation had taken place and they have been onboard for any warranty issues as well as changes 
-              at the owners request.  I would highly recommend them for any up and coming projects that need to be carried 
-              out to a ‘super yacht’ standard and with professionalism that surpasses all other companies in the yacht business.” - Captain Christopher Grant, M/Y Mea Culpa
-              </p>
+            <div className={styles.flexSupport}>
+              <div className={styles.support}>
+                <h2>Trusted Support</h2>
+                <p>Customer Service is what makes A/V Concepts and Design stand out from our competitors.</p>
+                <p> 
+                  Neither the size nor the scope of a project is our concern, rather it is our professional manner, 
+                  and our proven ability to work with the project team to get the job done on time, and as expected. 
+                  We offer 24 hour, year-round service and support.
+                </p>
+                <p>Worldwide Support. Representation in Washington & Florida.</p>
+                <p className={styles.quote}>
+                  "Entertainment systems are so advanced you absolutely need the most knowledgeable system designer and support available.  
+                  AV Concepts has proven time and time again the ability to bring together a world of entertainment that caters to the customer’s needs.  
+                  Knowledge and support have been key factors to keeping things up to date and performing with outstanding results. After working 
+                  with Steve & Cindy on both new & refit projects for many years it has been a pleasure to have their support.” - Captain John Doty, M/Y Onika
+                </p>
+              </div>
+              <img src={maramarack}></img>
             </div>
           </div>
       </div>
