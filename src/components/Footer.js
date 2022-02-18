dcf4c095f6bb2699480624f70e29adf4733ebeb3
Scott Ammon <scott.o.ammon@gmail.com>
2022-02-18 09:55:59 -0800

    fix(footer): update address

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,13 +13,13 @@ const Footer = () => (
         <Link className={styles.footerLink} to="/contact">Contact</Link>
       </div>
       <div className={styles.contact}>
-        <p className={styles.number}>AV Concepts & Design</p>
-        <p className={styles.number}>15336 NE 96th Place, #C-3</p>
-        <p className={styles.number}>Redmond, WA 98052 USA</p>
+        <p className={styles.number}>Audio Video Concepts & Design</p>
+        <p className={styles.number}>4701 SW Admiral Way #104</p>
+        <p className={styles.number}>Seattle, WA  98116</p>
         <p className={styles.number}>Phone:	+1 (425) 881-5621</p>
       </div>
     </div>
-    <p className = {styles.copyright}>&copy; 2018 SOA | Built with React</p> 
+    <p className = {styles.copyright}>&copy; 2022 SOA | Built with React</p>
   </div>
 )
 
